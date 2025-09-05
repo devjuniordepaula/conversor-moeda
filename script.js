0e503606be1666b17b103a2f3dcad7f14f6348e8
Enivaldo Junior <devjuniordepaula@gmail.com>
2025-09-05 16:24:16 -0300

    teste

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-
 const valor = document.querySelector('#valor');
 const de = document.querySelector('#de');
 const para = document.querySelector('#para');
 const resultado = document.querySelector('#resultado');
 const conversor = document.querySelector('#conversor');
 
-
 const taxas = {
-    brl: {brl: 1, usd: 0.18, euro: 1.16},
-    usd: {brl: 5.47, usd: 1, euro: 0.86},
-    euro: {brl: 6.67, usd: 1.16, euro: 1}
+  brl: {brl: 1, usd: 0.18, eur: 0.16},
+  usd: {brl: 5.47, usd: 1, eur: 0.86},
+  eur: {brl: 6.37, usd: 1.16, eur: 1}
 };
 
 conversor.addEventListener('click', () => {
 
-    const valorInserido = valor.value;
+  const valorInserido = parseFloat(valor.value);
+  const valorDe = de.value;
+  const valorPara = para.value;
+
+  if(isNaN(valorInserido) || valorInserido <= 0){
+    resultado.textContent = 'Digite um valor válido!';
+    return;
+  };
+
+  const taxa = taxas[valorDe][valorPara];
+
+  const conversao = valorInserido * taxa;
+
+  resultado.textContent = `${valorDe.toUpperCase()} ${valorInserido} = ${valorPara.toUpperCase()} ${conversao.toFixed(2)}`;
 
 });
